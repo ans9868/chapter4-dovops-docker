aac4ca131618272daa3aab0d170a661f587b0433
Adel Nour <ans9868@nyu.edu>
2025-06-27 16:20:53 -0400

    4:20 pm edit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello World! updated 4:05pm</h1>");
+  res.send("<h1>Hello World! updated 4:20pm</h1>");
 });
 
 const PORT = 8080;
