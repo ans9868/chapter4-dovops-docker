e1d1ea4691088279709e433b8fd5e5175a20f9ea
Adel Nour <ans9868@nyu.edu>
2025-06-29 17:37:25 -0400

    finished 3.3

---
+++ builder.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# crash if any line doens't return 0 ie doesn't execute correctly
+set -e
+
+# if there wasn't 2 user inputs then display this message
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <github-repo> <dockerhub-repo>"
+  echo "Example: $0 ans9868/chapter4-dovops-docker nour333/chapter4-express-app"
+  exit 1
+fi
+
+GITHUB_REPO=$1
+DOCKERHUB_REPO=$2
+
+# clone the github and go in the folder so we can build it
+git clone https://github.com/$GITHUB_REPO temp-repo
+cd temp-repo
+
+#build the docker image which we can then push
+docker build -t $DOCKERHUB_REPO .
+
+#now we can clone the repo
+docker push $DOCKERHUB_REPO
+
+#finally clean up cloned repo
+cd ..
+rm -rf temp-repo
+
+echo "Successfully built and pushed $DOCKERHUB_REPO"
